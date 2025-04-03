dc26a5d8468b4a2cf4ae0dd76e05cc76dd5694a4
Aaditya Singh <as3870955@gmail.com>
2025-04-04 00:35:10 +0530

    Fixed Authentication

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,21 +1,44 @@
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
-        <div className="wrapper flex items-center justify-between">
-            <Link href="/" className="w-36">
-            <Image src="/assets/images/logo.svg" width={138} height={38}
-            alt="evently logo"/>
-            </Link>
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" 
+            width={128} 
+            height={38}
+            alt="Evently logo" 
+          />
+        </Link>
 
-            <div className="flex w-3 justify-end gap-3">
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
-            </div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
