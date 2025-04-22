f8bff29d0d64d5ec96b6a33c05387641bf16a743
Aaditya Singh <as3870955@gmail.com>
2025-04-22 15:30:20 +0530

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
+  clerkId: { type: String, required: false, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: false},
   firstName: { type: String, required: true },
